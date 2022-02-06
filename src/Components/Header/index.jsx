a2c9248cb0c7359db7156a87184949979f4237f6
Oleg672 <oleg6080826@mail.ru>
2022-02-06 22:34:53 +0300

    add nav

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../Header/style-module.css';
 function Header() {
+
+    const [visability,setVisability] = useState(true)
+    function visabilityNawigate(){
+      var a=  document.getElementById('header-menu').style.display=`${visability===true? 'flex':'none'}`;
+      setVisability(!visability)
+      console.log(visability);
+    }
 
     return ( <div className='container col-10 col-xl-8'>
         < div className="header" >
            
                 <h1 className='header__title'>Alivio</h1>
-                <ul className="header-menu">
+               
+                <ul className="header-menu" id='header-menu'>
                     <li className="header-menu__item"><a href="">Why Alivio</a></li>
                     <li className="header-menu__item"><a href="">Solutions</a></li>
                     <li className="header-menu__item"><a href="">Community</a></li>
@@ -16,6 +24,7 @@ function Header() {
                     <a href="#" className="header-autorize">Sign-in</a>
                     <button className='btnFullSite'>Start Trial</button>
                 </div>
+                <button className='header__bnt-naw' onClick={visabilityNawigate} ><span></span><span></span><span></span></button>
             </div >
         </div>
 
