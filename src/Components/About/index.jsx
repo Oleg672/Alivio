5cda00393eb77f9c44232c43d48c1290cb7d0191
Oleg672 <oleg6080826@mail.ru>
2022-02-08 14:43:56 +0300

    responsive

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -7,12 +7,12 @@ function About() {
     return (
         <section className="About row">
                 {/* <div className="AboutInner col-10 col-xl-8"> */}
-                    <div className="About__title col-10 col-md-10 col-xl-6">
+                    <div className="About__title col-10 col-md-10 col-lg-4 col-xl-6">
                         <h2>Let's hear about Kayla's success story</h2>
                         <p>See how well Alivio works in a real customer’s life. </p>
                         <button className='btnFullSite' >Let’s get started</button>
                     </div>
-                    <div className="About__video col-10 col-md-10 col-xl-6">
+                    <div className="About__video col-10 col-md-10 col-lg-6 col-xl-6">
                         <img src={Video1} alt="" />
                         <button className="About__video-btn"><img className="About__video-btnIMG" src={Play} alt="" /></button>
                     </div>
--- src/Components/Works/index.jsx
+++ src/Components/Works/index.jsx
@@ -9,35 +9,37 @@ function Works() {
 
         <section className="works row">
             <div className='container col-10 col-xl-8'>
-                
-                    <div className="works__title col-12 .col-md-6">
-                        <span>How it works</span>
+                <div className="inner row">
+                    <div className="works__title col-12 .col-md-6 col-lg-5">
+                        {/* <span>How it works</span> */}
                         <h2>Understand & Release the stress in 3 steps</h2>
                     </div>
 
-                    <p className="works__description col-12 .col-md-6">Alivio offers as many journals it takes, tackling different areas such as anxiety, overwhelmedness, sadness, or anger, and a variety of personal causes, to help you become aware of your emotions, and guide you in how to manage stress.</p>
+                    <p className="works__description col-12 .col-md-6 col-lg-6">Alivio offers as many journals it takes, tackling different areas such as anxiety, overwhelmedness, sadness, or anger, and a variety of personal causes, to help you become aware of your emotions, and guide you in how to manage stress.</p>
 
-                    <ul className="works__list row">
-                        <li className="works__item col-12 col-lg-4">
-                            <span className="works__itemNUM">01</span>
-                            <h6 className="works__itemTitle" >Personalize</h6>
-                            <p className="works__itemDescription">Answer a quick survey about how you express yourself, what causes you stress, and what area would you like to work on. This way, we can fully personalize your journal!</p>
-                            <img className="works__itemIMG" src={Img1} alt="" />
-                        </li>
-                        <li className="works__item col-12 col-lg-4">
-                            <span className="works__itemNUM">02</span>
-                            <h6 className="works__itemTitle" >Write & Understand</h6>
-                            <p className="works__itemDescription">Write, draw, reflect, understand. Alivio will guide you through the prompts and will help you manage your stress!</p>
-                            <img className="works__itemIMG" src={Img2} alt="" />
-                        </li>
-                        <li className="works__item col-12 col-lg-4">
-                            <span className="works__itemNUM">03</span>
-                            <h6 className="works__itemTitle" >Alivio!</h6>
-                            <p className="works__itemDescription">Now you are aware, and have a way to manage and overcome your own stress.What are you waiting for? Alivio today!</p>
-                            <img className="works__itemIMG" src={Img3} alt="" />
-                        </li>
-                    </ul>
-                
+                </div>
+
+                <ul className="works__list row">
+                    <li className="works__item col-12 col-lg-4">
+                        <span className="works__itemNUM">01</span>
+                        <h6 className="works__itemTitle" >Personalize</h6>
+                        <p className="works__itemDescription">Answer a quick survey about how you express yourself, what causes you stress, and what area would you like to work on. This way, we can fully personalize your journal!</p>
+                        <img className="works__itemIMG" src={Img1} alt="" />
+                    </li>
+                    <li className="works__item col-12 col-lg-4">
+                        <span className="works__itemNUM">02</span>
+                        <h6 className="works__itemTitle" >Write & Understand</h6>
+                        <p className="works__itemDescription">Write, draw, reflect, understand. Alivio will guide you through the prompts and will help you manage your stress!</p>
+                        <img className="works__itemIMG" src={Img2} alt="" />
+                    </li>
+                    <li className="works__item col-12 col-lg-4">
+                        <span className="works__itemNUM">03</span>
+                        <h6 className="works__itemTitle" >Alivio!</h6>
+                        <p className="works__itemDescription">Now you are aware, and have a way to manage and overcome your own stress.What are you waiting for? Alivio today!</p>
+                        <img className="works__itemIMG" src={Img3} alt="" />
+                    </li>
+                </ul>
+
             </div>
         </section>
 
